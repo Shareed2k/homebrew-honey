60343fcb223ae74af0580160730c814e74d105c4
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-11 11:13:33 +0200

    Brew formula update for honey version v0.1.2

--- honey.rb
+++ honey.rb
@@ -5,16 +5,16 @@
 class Honey < Formula
   desc "DevOps tool to help find an instance in sea of clouds."
   homepage "https://github.com/shareed2k/honey"
-  version "0.1.1"
+  version "0.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/shareed2k/honey/releases/download/v0.1.1/honey_Darwin_x86_64.zip"
-    sha256 "ff09eede3bc41325b1f0cf30894bed42779b9336fbeccbf6749c7179e0752c7a"
+    url "https://github.com/shareed2k/honey/releases/download/v0.1.2/honey_Darwin_x86_64.zip"
+    sha256 "fe40998ccd757b4d7220d2f69fce24926c6a8b98c413c2234a17aaaf23789b63"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/shareed2k/honey/releases/download/v0.1.1/honey_Linux_x86_64.zip"
-    sha256 "658eea7e2e895599ccc556331929523c08d4f9317062fa7874aab34851e943f2"
+    url "https://github.com/shareed2k/honey/releases/download/v0.1.2/honey_Linux_x86_64.zip"
+    sha256 "66931faa1b3a717626c40b347d705276919a09487d1964dde6b9d0eff5a11eee"
   end
 
   def install
